b6abc0190dacf607a2a7caa9f759dc929d82c756
Cristina Mastellaro <cristina.mastellaro@accenture.com>
2025-01-07 12:06:00 +0100

    Update jenkinsfileguida.groovy
    blank spaces
    
    another blank space
    
    add a line

--- jenkinsfileguida.groovy
+++ jenkinsfileguida.groovy
@@ -1,7 +1,7 @@
 @Library("PrimaLibrary") _
 pipeline {
 	agent any
-	stages {
+	/*stages {
 		stage('Pulizia') {
 			steps {
 				cleanWs()
@@ -45,5 +45,35 @@ pipeline {
 				}
 			}
 		}
+	}*/
+
+
+	environment {
+	        DOCKER_RUN_PARAMS = "-u root"
+		PROJECT_URL = "www.ciao.it"
 	}
+
+	parameters {
+	    string(name: 'TARGET_BRANCH_NAME', defaultValue: '', description: 'The name of the target branch of the pull request', trim: true)
+	    string(name: 'SOURCE_BRANCH_NAME', defaultValue: env.SOURCE_BRANCH, description: '(Optional) The name of the source branch of the pull request', trim: true)
+	    string(name: 'RELEASE_VERSION', defaultValue: '', description: 'The name of the release in the format (i.e. 2020.12.16)', trim: true)
+	    choice(name: 'TARGET_ENVIRONMENT', choices: [env.TARGET_ENV_1, env.TARGET_ENV_2], description: env.TARGET_ENVS_DESCRIPTION)
+	}
+
+	stages {
+	        stage('Print') {
+	            steps {
+	                script {
+	                    echo PROJECT_URL
+	                    echo env.PROJECT_URL
+	                    echo SOURCE_BRANCH_NAME
+	                    echo env.SOURCE_BRANCH
+	                    echo TARGET_ENVIRONMENT
+	                }
+	            }
+	        }
+    	} 
 }
+
+
+
